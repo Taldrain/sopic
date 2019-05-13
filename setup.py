06439fda7937d8ec2572744e9550826462e678c8
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2019-05-13 14:55:28 +0200

    add MIT license

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(name='sopic',
     version='0.0.0',
     description='Helper for manufacturing station',
     install_requires=['PyQt5', 'colorlog'],
-    packages=['sopic']
+    packages=['sopic'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
 )
