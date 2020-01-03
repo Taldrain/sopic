f73bf567a1a4569c4a01a6716d1aa22949520790
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2020-01-03 14:59:17 +0100

    freeze dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
 setup(name='sopic',
-    version='0.0.0',
+    version='0.0.1',
     description='Helper for manufacturing station',
-    install_requires=['PyQt5', 'colorlog'],
+    install_requires=[
+        'PyQt5==5.14.0',
+        'colorlog==4.1.0',
+    ],
     packages=['sopic'],
     classifiers=[
         "Programming Language :: Python :: 3",
