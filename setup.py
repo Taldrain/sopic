0c810fd1e38665e1b32ba3f2761c5a7a055f1164
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2020-01-03 15:31:11 +0100

    change pyqt5 version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='sopic',
-    version='0.0.1',
+    version='0.0.2',
     description='Helper for manufacturing station',
     install_requires=[
-        'PyQt5==5.14.0',
+        'PyQt5==5.13.1',
         'colorlog==4.1.0',
     ],
     packages=['sopic'],
